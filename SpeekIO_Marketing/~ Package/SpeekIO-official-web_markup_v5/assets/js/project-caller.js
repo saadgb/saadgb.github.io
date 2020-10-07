fa32032395fe280f03dde5804fc1ed27e1abbcbd
Saad <saad.tariq88@gmail.com>
2020-10-07 16:53:49 +0500

    Speekio marketing and portal

---
+++ SpeekIO_Marketing/~ Package/SpeekIO-official-web_markup_v5/assets/js/project-caller.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    videoModal(); 
+    gotoTop();
+    toast();
+});
---
+++ SpeekIO_Portal/assets/js/speekio-caller.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    
+   
+   
+    chart1(); 
+    chart2();  
+    chart3(); 
+    /*customScroller();*/
+    sidebarToggle();
+    nestedToggle(); 
+    dropdownStatic();
+    tableRowClickable();
+    layoutReviewer();
+    tooltip();
+    popover();
+    validations();   
+     candidateTimer();
+    treeSelect();
+      openNav();
+    closeNav();
+    
+     richTextEditor();
+	charttest();
+    printProfile();    
+  
+});
