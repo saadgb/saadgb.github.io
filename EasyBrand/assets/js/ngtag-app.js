b417b2905584d39fada745098c782735144d87a1
Muhammad Saad <saad.tariq88@gmail.com>
2024-11-25 13:21:12 +0500

    Delete EasyBrand directory

--- EasyBrand/assets/js/ngtag-app.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('plunker', ['ngTagsInput']);
-
-app.controller('MainCtrl', function($scope, $http) {
-  $scope.tags = [
-    { text: 'Tag1' },
-    { text: 'Tag2' },
-    { text: 'Tag3' }
-  ];
-});
